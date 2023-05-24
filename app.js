d9e1c6cf2740aebb77c8a6abc9df2bb83abc8ae4
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-05-24 20:07:18 -0300

    docs: modificando app

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const calculadora = require('./calculadora')
-let resultado = 0
-
+const calculadora = require("./calculadora");
+let resultado = 0;
 switch (process.argv[2]) {
-    case "sumar":
-        resultado = calculadora.sumar(process.argv[3],process[4])
+  case "sumar":
+    resultado = calculadora.sumar(process.argv[3], process.argv[4]);
+    break;
+  case "restar":
+    resultado = calculadora.restar(process.argv[3], process.argv[4]);
+    break;
+  case "multiplicar":
+    resultado = calculadora.multiplicar(process.argv[3], process.argv[4]);
+    break;
+  case "dividir":
+    resultado = calculadora.dividir(process.argv[3], process.argv[4]);
+    break;
+    case undefined :
+        resultado = "ERROR: tiene que dar una operacion"
         break;
-        case "restar":
-        resultado = calculadora.sumar(process.argv[3],process[4])
-        break;
-        case "multiplicar":
-        resultado = calculadora.sumar(process.argv[3],process[4])
-        break;
-        case "dividir":
-        resultado = calculadora.sumar(process.argv[3],process[4])
-        break;
-    default:
-        resultado = "ERROR: comando incorrecto";
-        break;
-    }
+  default:
+    resultado = "ERROR: comando incorrecto";
+    break;
+}
 
 console.log(resultado);
